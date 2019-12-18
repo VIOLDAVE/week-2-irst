0b140777d54446e58b683dcb2e54ff4b26de6394
violdave <aworviola2@gmail.com>
2019-12-18 14:37:29 +0300

    initial commit

---
+++ age.js
@@ -0,0 +1,25 @@
+function calculateAge()
+{
+let birthyear =prompt("please enter year of birth")
+let d  =new Date()
+let y= (2019);
+let calculateAge=2019-birth_year;
+}
+if (age<= 18)
+ {
+    user ="You are a minor";
+
+}
+ elseif (age>=18 && age<= 36) 
+{
+    user="You are a youth";
+
+} 
+elseif (age > 36)
+ {
+    user=" You are an elder";
+} 
+{
+alert(user);
+}
+
---
+++ objectChallenge.js
@@ -0,0 +1,24 @@
+function square (x){
+    return(x*x);
+};
+let squares={
+"1":square(1),
+"2":square(2),
+"3":square(3), 
+"4":square(4),
+"5":square(5),
+"6":square(6),
+"7":square(7),
+"8":square(8),
+"9":square(9),
+"10":square(10),
+"11":square(11),
+"12":square(12),
+"13":square(13),
+"14":square(14),
+"15":square(15)
+}
+console.log(squares);
+
+
+
